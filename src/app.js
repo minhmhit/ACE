9a173696c44b488f02e1a52eecdae49bb7c35001
minhmh <minh.maihoang.71@gmail.com>
2025-11-10 20:02:03 +0700

    receipt

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ import orderRoutes from "./routes/order.routes.js";
 import inventoryRoutes from "./routes/inventory.routes.js";
 import supplierRoutes from "./routes/supplier.routes.js";
 import couponRoutes from "./routes/coupon.routes.js";
-import importRoutes from "./routes/import.routes.js"
+import importRoutes from "./routes/import.routes.js";
+import receiptRoutes from "./routes/receipt.routes.js";
 
 // Load env vars
 dotenv.config();
@@ -30,12 +31,12 @@ app.use("/api/v1/orders", orderRoutes);
 app.use("/api/v1/inventory", inventoryRoutes);
 app.use("/api/v1/suppliers", supplierRoutes);
 app.use("/api/v1/coupons", couponRoutes);
-app.use("api/v1/imports", importRoutes);
+app.use("/api/v1/imports", importRoutes);
+app.use("/api/v1/receipts", receiptRoutes);
 // Routes
 app.get("/", (req, res) => {
   res.json({ message: "Coffee Shop API" });
 });
-
 
 // Error handler
 // app.use(errorHandler);
