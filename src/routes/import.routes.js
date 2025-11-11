9f2030892d8fd02cb1df46410f0b00d2d61f02f5
minhmh <minh.maihoang.71@gmail.com>
2025-11-11 18:49:30 +0700

    import

--- src/routes/import.routes.js
+++ src/routes/import.routes.js
@@ -5,19 +5,23 @@ import {
   updatePaymentStatusValidation,
 } from "../middlewares/importValidation.js";
 import { validateRequest } from "../middlewares/validateRequest.js";
+import { authenticate, authorize } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+const importAuth = [authenticate, authorize(1,3)];
+
 // Lấy danh sách phiếu nhập hàng (có phân trang và lọc)
-router.get("/", ImportController.getAllImports);
+router.get("/", importAuth, ImportController.getAllImports);
 
 // Lấy chi tiết một phiếu nhập hàng
-router.get("/:id", ImportController.getImportById);
+router.get("/:id", importAuth, ImportController.getImportById);
 
 // Tạo phiếu nhập hàng mới
 router.post(
-  "/",
+  "/add",
   createImportValidation,
+  importAuth,
   validateRequest,
   ImportController.createImport
 );
@@ -26,11 +30,12 @@ router.post(
 router.patch(
   "/:id/status",
   updatePaymentStatusValidation,
+  importAuth,
   validateRequest,
   ImportController.updatePaymentStatus
 );
 
 // Xoá phiếu nhập hàng (chỉ cho phép xoá khi chưa thanh toán)
-router.delete("/:id", ImportController.deleteImport);
+router.delete("/:id", importAuth, ImportController.deleteImport);
 
 export default router;
